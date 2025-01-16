14217208c617484874a9da2c800a4d97d79ad358
daniel_gomes <danielcerqueira7747@gmail.com>
2025-01-16 19:06:07 -0300

    feat: added status urls

--- api/urls.py
+++ api/urls.py
@@ -19,6 +19,7 @@ account_link_router = NestedDefaultRouter(router, r'account', lookup='account')
 account_link_router.register(r'link', LinkViewSet, basename='account-link')
 
 urlpatterns = [
+    path('api/v1/', include('api.status.urls')),
     path('api/v1/subscription/', include('api.subscriptions.urls')),
     path('api/v1/', include('api.profile_user.urls')),
     path('api/v1/auth/', include('api.auth.urls')),
