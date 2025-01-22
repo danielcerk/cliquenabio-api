e5ccf1711e626e5a33010664d8833ee60aa83be4
daniel_gomes <danielcerqueira7747@gmail.com>
2025-01-22 10:46:38 -0300

    feat: updated subscription

--- api/subscriptions/signals.py
+++ api/subscriptions/signals.py
@@ -4,20 +4,11 @@ from django.db.models.signals import post_save
 
 from .models import Subscription
 
-import stripe
-
 User = settings.AUTH_USER_MODEL
-stripe.api_key = settings.STRIPE_SECRET_KEY
 
 @receiver(post_save, sender=User)
 def create_subscription_user(sender, instance, created, **kwargs):
     
     if created and not Subscription.objects.filter(user=instance).exists():
-
-        customer = stripe.Customer.create(
-            email=instance.email,
-            name=instance.name
-        )
         
-        Subscription.objects.create(user=instance,
-            stripe_customer_id=customer['id'])
+        Subscription.objects.create(user=instance)
--- api/subscriptions/urls.py
+++ api/subscriptions/urls.py
@@ -1,27 +1,7 @@
 from django.urls import path
-from .views import (
-    CancelSubscriptionAPIView, 
-    CreateSubscriptionAPIView,
-    AttachPaymentMethodAPIView,
-    UpdatePlanAPIView,
-    StripeWebhookAPIView,
-    GetPlansAPIView,
-    PlanUserAPIView
-)
+from .views import StripeWebhookAPIView
 
 urlpatterns = [
-    path('plans/', GetPlansAPIView.as_view(), name='get-plans-stripe'), # Planos
 
-
-    ########################## COLOCAR ID DO USUARIO NA ROTA ##########################
-
-    path('plan/', PlanUserAPIView.as_view(), name='plan-user-stripe'), # Plano do usuário
-    path('create-subscription/', CreateSubscriptionAPIView.as_view(), name='create-subscription'), # Fazer assinatura
-    path('cancel-subscription/', CancelSubscriptionAPIView.as_view(), name='cancel-subscription'), # Cancelar assinatura
-    path('attach-payment-method/', AttachPaymentMethodAPIView.as_view(), name='attach-payment-method'), # Metodo de pagamento
-    path('update-plan/', UpdatePlanAPIView.as_view(), name='update-plan'), # Atualizar assinatura
-
-    ###################################################################################
-
-    path('stripe/webhook/', StripeWebhookAPIView.as_view(), name='stripe-webhook'), # Monitorar
+    path('stripe/webhook/', StripeWebhookAPIView.as_view(), name='stripe-webhook'),
 ]
