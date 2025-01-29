695c8a409d7e7e8a0ea105b2d28187af36963de6
Daniel Gomes <76263577+danielcerk@users.noreply.github.com>
2025-01-29 09:38:39 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
 
     DATABASES = {
         'default': {
