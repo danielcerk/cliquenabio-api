f2dba9b6570f41cee812759b95a9246cc1b5163d
daniel_gomes <danielcerqueira7747@gmail.com>
2025-02-07 21:15:32 -0300

    feat: updated signals

--- api/theme/signals.py
+++ api/theme/signals.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
 from django.db.models.signals import post_save, pre_delete
@@ -10,11 +11,16 @@ User = settings.AUTH_USER_MODEL
 
 @receiver(post_save, sender=User)
 def create_theme_user(sender, instance, created, **kwargs):
-    
     if created and not ThemeUser.objects.filter(user=instance).exists():
-
-        theme_global = ThemeGlobal.objects.get(pk=1)
         
-        ThemeUser.objects.create(
-            user=instance, theme=theme_global
+        theme_global, created = ThemeGlobal.objects.get_or_create(
+            pk=1,
+            defaults={
+                "name": "Light",
+                "background_color": "#fff",
+                "foreground_color": "#000",
+                "font_family": "Open Sans",
+            }
         )
+
+        ThemeUser.objects.create(user=instance, theme=theme_global)
