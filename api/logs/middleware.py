03a7d6428d88fabc6452d06be80ecf84238bc2a1
daniel_gomes <danielcerqueira7747@gmail.com>
2025-02-10 11:08:30 -0300

    fix: middleware to accept image

--- api/logs/middleware.py
+++ api/logs/middleware.py
@@ -5,25 +5,19 @@ import json
 class UserActivityLoggerMiddleware:
 
     def __init__(self, get_response):
-
         self.get_response = get_response
 
     def __call__(self, request):
-
-        if request.body:
-
+        # Verifique o tipo de conteúdo para evitar tentar decodificar arquivos binários
+        if request.content_type == 'application/json' and request.body:
             try:
-
                 request.data = json.loads(request.body.decode('utf-8') or "{}")
-
             except json.JSONDecodeError:
-
                 request.data = {}
 
         response = self.get_response(request)
 
         if request.user.is_authenticated and not request.path.startswith("/static/"):
-
             action_details = self.get_action_details(request)
 
             if action_details:
@@ -40,45 +34,33 @@ class UserActivityLoggerMiddleware:
         return response
 
     def get_action_details(self, request):
-
         method = request.method
         object_id = None
         model_name = None
 
         try:
-
             if method in ["POST", "PUT", "PATCH", "DELETE"]:
-
                 data = getattr(request, "data", request.POST or {})
 
                 object_id = data.get("id") or data.get("pk") or None
-
                 path_parts = request.path.strip("/").split("/")
 
                 if len(path_parts) > 1 and path_parts[-1].isdigit():
-
                     object_id = path_parts[-1]
                     model_name = path_parts[-2]
 
                 elif "change" in path_parts and len(path_parts) > 4:
-
                     object_id = path_parts[-2]
                     model_name = path_parts[-3]
 
                 if method == "POST":
-
                     return f"Criou um novo {model_name or 'registro'} (ID: {object_id or 'N/A'})"
-                
                 elif method in ["PUT", "PATCH"]:
-
                     return f"Atualizou o {model_name or 'registro'} (ID: {object_id or 'N/A'})"
-                
                 elif method == "DELETE":
-
                     return f"Removeu o {model_name or 'registro'} (ID: {object_id or 'N/A'})"
         
         except json.JSONDecodeError:
-
             pass
 
         return None
