0c609f0b5f227b1f454ea116fe61942c13354db1
daniel_gomes <danielcerqueira7747@gmail.com>
2025-02-26 10:40:54 -0300

    feat: added signals to update slug of name

--- api/profile_user/signals.py
+++ api/profile_user/signals.py
@@ -1,10 +1,10 @@
+from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
-
 from django.db.models.signals import post_save
+from django.utils.text import slugify
+from django.conf import settings
 
 from .models import Profile
-
-from django.conf import settings
 
 User = settings.AUTH_USER_MODEL
 
@@ -14,3 +14,14 @@ def create_profile_user(sender, instance, created, **kwargs):
     if created and not Profile.objects.filter(by=instance).exists():
         
         Profile.objects.create(by=instance)
+
+@receiver(post_save, sender=User)
+def update_slug(sender, instance, **kwargs):
+
+    profile = get_object_or_404(Profile, by=instance)
+
+    slug_name = slugify(instance.name)
+
+    profile.slug = slug_name
+
+    profile.save()
