f7227624ec2e8178f4e8ab0270a63bdc99e488ec
daniel_gomes <danielcerqueira7747@gmail.com>
2025-03-09 10:21:55 -0300

    feat: blocked admin view in production

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+
 from dj_rest_auth.views import PasswordResetConfirmView, PasswordResetView
 
 urlpatterns = [
@@ -8,7 +10,11 @@ urlpatterns = [
             name='password_reset_confirm'),
     path('api/v1/auth/password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
     path('api/v1/auth/password/reset/confirm/', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('api/v1/auth/', include('dj_rest_auth.urls')),
 ]
+
+if settings.DEBUG:
+
+    urlpatterns.append(path("admin/", admin.site.urls))
