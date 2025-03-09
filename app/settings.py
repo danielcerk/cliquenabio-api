90db610e0acaecbca484e504d20cb86e6b0c95fe
daniel_gomes <danielcerqueira7747@gmail.com>
2025-03-09 10:22:24 -0300

    feat: api in json format only

--- app/settings.py
+++ app/settings.py
@@ -235,6 +235,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     #'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     #'PAGE_SIZE': 150,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
